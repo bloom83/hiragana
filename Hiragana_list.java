7ebc63fecbb62e552edd7ead606f49385d0ad60a
bloom83 <bloom.est.2019@gmail.com>
2022-07-21 21:04:51 +0900

    hiraganaList_data 1st

---
+++ Hiragana_list.java
@@ -0,0 +1,165 @@
+package hiragana
+import java.util.ArrayList;
+
+public class Hiragana_list{
+    private ArrayList<String> hiragana = new ArrayList<String>();
+
+    hiragana.add("あ");
+    hiragana.add("い");
+    hiragana.add("う");
+    hiragana.add("え");
+    hiragana.add("お");
+
+    hiragana.add("か");
+    hiragana.add("き");
+    hiragana.add("く");
+    hiragana.add("け");
+    hiragana.add("こ");
+
+    hiragana.add("さ");
+    hiragana.add("し");
+    hiragana.add("す");
+    hiragana.add("せ");
+    hiragana.add("そ");
+
+    hiragana.add("た");
+    hiragana.add("ち");
+    hiragana.add("つ");
+    hiragana.add("て");
+    hiragana.add("と");
+
+    hiragana.add("な");
+    hiragana.add("に");
+    hiragana.add("ぬ");
+    hiragana.add("ね");
+    hiragana.add("の");
+
+    hiragana.add("は");
+    hiragana.add("ひ");
+    hiragana.add("ふ");
+    hiragana.add("へ");
+    hiragana.add("ほ");
+
+    hiragana.add("ま");
+    hiragana.add("み");
+    hiragana.add("む");
+    hiragana.add("め");
+    hiragana.add("も");
+
+    hiragana.add("や");
+    hiragana.add("ゆ");
+    hiragana.add("よ");
+
+    hiragana.add("ら");
+    hiragana.add("り");
+    hiragana.add("る");
+    hiragana.add("れ");
+    hiragana.add("ろ");
+
+    hiragana.add("わ");
+    hiragana.add("を");
+    hiragana.add("ん");
+
+    hiragana.add("きゃ");
+    hiragana.add("きゅ");
+    hiragana.add("きょ");
+
+    hiragana.add("しゃ");
+    hiragana.add("しゅ");
+    hiragana.add("しぇ");
+    hiragana.add("しょ");
+
+    hiragana.add("ちゃ");
+    hiragana.add("ちゅ");
+    hiragana.add("ちぇ");
+    hiragana.add("ちょ");
+
+    hiragana.add("にゃ");
+    hiragana.add("にゅ");
+    hiragana.add("にょ");
+
+    hiragana.add("みゃ");
+    hiragana.add("みゅ");
+    hiragana.add("みょ");
+
+    hiragana.add("りゃ");
+    hiragana.add("りゅ");
+    hiragana.add("りょ");
+
+    hiragana.add("てぃ");
+    hiragana.add("っ");
+
+    hiragana.add("ぁ");
+    hiragana.add("ぃ");
+    hiragana.add("ぅ");
+    hiragana.add("ぇ");
+    hiragana.add("ぉ");
+
+    hiragana.add("ゃ");
+    hiragana.add("ゅ");
+    hiragana.add("ょ");
+
+    private ArrayList<String> dakuon = new ArrayList<String>();
+    
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+
+    dakuon.add("");
+    dakuon.add("");
+    dakuon.add("");
+
+    dakuon.add("");
+
+    private ArrayList<String> handaku = new ArrayList<String>();
+
+    handaku.add("");
+    handaku.add("");
+    handaku.add("");
+    handaku.add("");
+    handaku.add("");
+
+    handaku.add("");
+    handaku.add("");
+    handaku.add("");
+
+
+
+
+
+
+
+
+}
